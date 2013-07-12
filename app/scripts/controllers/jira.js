0fdfe1005f61d388c88fff065c236b30f3f20f01
Sweta Vajjhala <sweta.vajjhala@gmail.com>
2013-07-11 19:50:33 -0700

    JIRA widget basic styling and Angular wiring works.

--- app/scripts/controllers/jira.js
+++ app/scripts/controllers/jira.js
@@ -3,5 +3,18 @@
 angular.module('myvestDashboardApp')
     .controller('JiraWidgetController', function ($scope) {
 
-    $scope.items = [{"key": "AMMO-12345"}, {"key": "AMMO-23456"}];
+    $scope.issues = [{"key": "AMMO-17507",
+                      "summary": "The generic KV DAO does not add all object indices on store()",
+                      "assignee": "Leland Garofalo",
+                      "reporter": "Leland Garofalo",
+                      "priority": "Blocker"},
+                     {"key": "AMMO-23456", "summary": "Whatevers"}];
+
+
+
+                                /** <td>{{issue.key}}</td>
+                                 <td>{{issue.summary}}</td>
+                                 <td>{{issue.assignee}}</td>
+                                 <td>{{issue.reporter}}</td>
+                                 <td>{{issue.priority}}</td> **/
 });
