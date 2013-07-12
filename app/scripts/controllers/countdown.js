e11215c091ad822cf70fea74ea44b9342932af39
Sweta Vajjhala <sweta.vajjhala@gmail.com>
2013-07-12 13:03:30 -0700

    Countdown styling from Felix & Matt. Added Jira status to view.

--- app/scripts/controllers/countdown.js
+++ app/scripts/controllers/countdown.js
@@ -2,7 +2,12 @@
 
 angular.module('myvestDashboardApp')
     .controller('CountdownWidgetController', function ($scope) {
-      $scope.eventDate = new Date();
+      if(!$scope.eventDate) {
+        $scope.eventDate = new Date("July 26, 2013 12:00:00");
+      }
+      if(!$scope.eventName) {
+        $scope.eventName = "13.0";
+      }
 
       onWidgetCompleted($scope, "countdown", function() {
         //Execute this code when widget has finished displaying
@@ -26,12 +31,27 @@ angular.module('myvestDashboardApp')
         var eventDate = moment($scope.eventDate);
         var today = moment(); // similar to new Date();
 
-        $scope.months=eventDate.diff(today, "months");
-        eventDate.subtract($scope.months, "months");
-        $scope.weeks=eventDate.diff(today, "weeks");
-        eventDate.subtract($scope.weeks, "weeks");
+//        $scope.months=eventDate.diff(today, "months");
+//        eventDate.subtract($scope.months, "months");
+//        $scope.weeks=eventDate.diff(today, "weeks");
+//        eventDate.subtract($scope.weeks, "weeks");
         $scope.days=eventDate.diff(today, "days");
         eventDate.subtract($scope.days, "days");
         $scope.hours=eventDate.diff(today, "hours");
+        eventDate.subtract($scope.hours, "hours");
+
+        $scope.minutes=eventDate.diff(today, "minutes");
+        eventDate.subtract($scope.minutes, "minutes");
+
+        $scope.seconds=eventDate.diff(today, "seconds");
+        eventDate.subtract($scope.seconds, "seconds");
       };
-});
+
+      $scope.countDown = 60;
+      var timer = setInterval(function(){
+        $scope.countDown--;
+        recalculateCountdown();
+        $scope.$apply();
+        console.log($scope.countDown);
+      }, 1000);
+    });
