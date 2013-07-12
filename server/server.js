ee03ae37ba610c279f1c70e1a888cc2ac74fbda3
Jeffrey Kwan <jeffureekwan@gmail.com>
2013-07-12 11:37:36 -0700

    Adding Bart API call

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var http = require('http');
+var parser = require('xml2js').parseString;
 
 io.sockets.on('connection', function (socket) {
 	socket.emit('news', { hello: 'world' });
@@ -15,6 +17,7 @@ io.sockets.on('connection', function (socket) {
 // use livereload middleware
 app.use(require('grunt-contrib-livereload/lib/utils').livereloadSnippet);
 
+
 app.get('/hello', function(req, res){
 	res.render('hello', {greeting: 'hello'});
 	// res.render('hello', {greeting: 'bonjour'});
@@ -25,3 +28,23 @@ exports = module.exports = server;
 exports.use = function() {
 	app.use.apply(app, arguments);
 };
+
+ app.get('/bart', function(req, res) {
+      http.get({
+         host: 'api.bart.gov',
+         path: '/api/etd.aspx?cmd=etd&orig=EMBR&key=MW9S-E7SL-26DU-VV8V'
+      }, function (response) {
+         var xml = '';
+         
+         response.on('data', function(chunk) {
+            xml += chunk;
+         });  
+
+         response.on('end', function(chunk) {
+            parser(xml, function (err, result) {
+               res.send(result);
+            });
+         });
+      });
+   });
+
